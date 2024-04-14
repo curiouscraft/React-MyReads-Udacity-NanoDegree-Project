01c19ceae88333fa0077e042943c34544c5076f9
Srikanth Doddi <srikanth.cloudarch@gmail.com>
2024-04-14 20:13:42 +0530

    Srikanth's React Project

---
+++ src/mockBooks.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "The Great Gatsby",
+    authors: ["F. Scott Fitzgerald"],
+    imageUrl:
+      "http://books.google.com/books/content?id=OeVfMiT4tTYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE71nwV8WaKSQAMcl9VvO4ZxXEO9hy4OAR0GxjuOjvJN_P8IXly3J5gxnR56g-yAXwzkUxkNZRpNkZBCoXub4YB4kmqj9WztBYEFSbgGokhZmCf8yNy8xIblCo4gp1FsU3Yp-LOVY&source=gbs_api"
+  },
+  {
+    title: "Crime and Punishment",
+    authors: ["Fyodor Dostoevsky"],
+    imageUrl:
+      "http://books.google.com/books/content?id=P3arDwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73BRWlCjvM-CZytTXKjTquKxvNHhDT2mJbTBm0q6r6QME3bP5htX8H-5sBtXigFvR4KzGlMYPb-sFh53NNPlkc6SGEPAZ3h-JZjiQorZNU6qOx6qgWQxpsGrxYT-wvXcFJ7-jtI&source=gbs_api"
+  },
+  {
+    title: "Pride and Prejudice",
+    authors: ["Jane Austen"],
+    imageUrl:
+      "http://books.google.com/books/content?id=s1g-ADy2N7MC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72tqkbTp42Q4GwrWQY9-u8mCgwB_t8oVysoU_XV-CFpzIkM0Uv9klKLjLMpEOJHFKo6lnvEAlH4Vteuxv2eJvH5S0FwXELGD1ZQUD_18sRJocZxkFGBt3gCfzZR8gxxvENrczNE&source=gbs_api"
+  }
+];
